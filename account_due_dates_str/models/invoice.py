d3c320e41ee528560eb336c391f8989133a13607
javierjcf <javierjcf@gmail.com>
2016-08-03 15:52:34 +0200

    [FIX]account_due_dates_str: Mejorado formato str, solo una vez el plazo de pago

--- account_due_dates_str/models/invoice.py
+++ account_due_dates_str/models/invoice.py
@@ -40,24 +40,21 @@ class AccountInvoice(models.Model):
                                                 self.move_id.id),
                                                ('date_maturity', "!=", False)],
                                               order="date_maturity asc")
+            payment_mode = False
+            if self.payment_mode_id:
+                payment_mode = self.payment_mode_id
+                expiration_dates_str += payment_mode.name + \
+                    u":\n"
             for line in move_lines:
                 date = time.strptime(line.date_maturity, "%Y-%m-%d")
                 date = datetime.fromtimestamp(mktime(date))
                 date = date.strftime("%d/%m/%Y")
-                payment_mode = False
-                if not payment_mode and self.payment_mode_id:
-                    payment_mode = self.payment_mode_id
                 expiration_dates_str += date + \
-                    " ------> " + \
+                    "                          " + \
                     str(self.type in ('out_invoice', 'in_refund') and
                         line.debit or (self.type in ('in_invoice',
                                                      'out_refund') and
-                        line.credit or 0))
-                if payment_mode:
-                    expiration_dates_str += " ------> " + payment_mode.name + \
-                        u"\n"
-                else:
-                    expiration_dates_str += "\n"
+                        line.credit or 0)) + "\n"
 
         self.expiration_dates_str = expiration_dates_str
 
