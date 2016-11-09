aa3a57d1f08ff43a51c60f7ce703d2e257caf21a
javierjcf <javierjcf@gmail.com>
2016-11-09 14:54:34 +0100

    [ADD]cmnt_custom_reports: Módulo que añade vencimientos y campos del comercial_partner_id a facturas y ventas

---
+++ cmnt_custom_reports/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'CMNT Custom Reports',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'account',
+        'account_payment_partner',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+        "views/report_invoice.xml",
+        "views/report_sale_order.xml",
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
