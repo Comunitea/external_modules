27ac2e8d249154428f255ef429b995375ada43b9
javierjcf <javierjcf@gmail.com>
2016-11-15 10:22:16 +0100

    [ADD]cmnt_global_discount: Añadido módulo descuento global en facturas y ventas

---
+++ cmnt_global_discount/README.rst
@@ -0,0 +1,23 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+====================
+CMNT Global Discount
+====================
+
+Adds global discount to invoice sales and reports.
+
+
+Credits
+=======
+
+Contributors
+------------
+* Comunitea
+* Javier Colmenero <javier@comunitea.com>
+
+Maintainer
+----------
+
+This module is maintained by the Comunitea <http://www.comunitea.comm>.
---
+++ cmnt_global_discount/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'CMNT Global Discount',
+    'version': '8.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'account',
+        'sale',
+        'stock_account',
+        'report'
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/account_invoice_view.xml",
+        "views/report_invoice.xml",
+        "views/report_sale_order.xml",
+    ],
+    "demo": [
+    ],
+    'test': [
+    ],
+    "installable": True
+}
