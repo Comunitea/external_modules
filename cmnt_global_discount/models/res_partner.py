4e514b81aa6ae4a641ecdfca49994b16a4532987
javierjcf <javierjcf@gmail.com>
2016-12-05 14:41:17 +0100

    [ADD]cmnt_global_discount: Añadido tipo y tasa al informe de ventas y factura

--- cmnt_global_discount/models/res_partner.py
+++ cmnt_global_discount/models/res_partner.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 # © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields
+from openerp import models, fields, _
 
 
 class ResPartner(models.Model):
 
     _inherit = 'res.partner'
 
-    discount_type = fields.Selection([('percent', 'Percentage'),
-                                      ('amount', 'Amount')],
+    discount_type = fields.Selection([('percent', _('Percentage')),
+                                      ('amount', _('Amount'))],
                                      string='Discount Type',
                                      help='Select discount type',
                                      default='percent')
