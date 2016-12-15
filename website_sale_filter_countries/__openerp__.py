815cfd9e660ce1cbc861dd921598ba90de9335da
jesusVMayor <jesus@comunitea.com>
2016-12-15 15:45:59 +0100

    [IMP]website_sale_filter_countries: Añadido módulo para filtrar los paises que se muestran en la tienda online.

---
+++ website_sale_filter_countries/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Website sale filter countries',
+    'summary': '',
+    'version': '8.0.1.0.0',
+    'category': 'Uncategorized',
+    'website': 'comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'website_sale',
+        'website_snippet_country_dropdown'
+    ],
+    'data': [
+        'views/res_country.xml',
+        'views/snippet.xml'
+    ],
+}
---
+++ website_sale_filter_countries/models/res_country.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, exceptions, _
+
+
+class ResCountry(models.Model):
+
+    _inherit = 'res.country'
+
+    website_available = fields.Boolean()
