517bb03beb95e5db52eae3ee90289f36dc708913
javierjcf <javierjcf@gmail.com>
2017-03-13 14:12:19 +0100

    [ADD]telesale: botones cabecera css

--- telesale/static/src/js/base_widgets.js
+++ telesale/static/src/js/base_widgets.js
@@ -3,6 +3,8 @@ odoo.define('telesale.BaseWidget', function (require) {
 
 var Widget = require('web.Widget');
 var models = require('telesale.models');
+var core = require('web.core');
+var _t = core._t;
 
 // This is a base class for all Widgets in the TS.
 var TsBaseWidget = Widget.extend({
@@ -23,28 +25,149 @@ var TsBaseWidget = Widget.extend({
 
 
 var SynchIconWidget = TsBaseWidget.extend({
-        template: "Synch-Notification-Widget",
-        init: function(parent, options){
-            options = options || {};
-            this._super(parent, options);
-        },
-        renderElement: function() {
-            var self = this;
-            this._super();
-            this.$el.click(function(){
-                self.ts_model.flush();
-            });
-        },
-        start: function(){
-            var self = this;
-            this.ts_model.bind('change:nbr_pending_operations', function(){
-                self.renderElement();
-            });
-        },
-        get_nbr_pending: function(){
-            return this.ts_model.get('nbr_pending_operations');
-        },
-    });
+    template: "Synch-Notification-Widget",
+    init: function(parent, options){
+        options = options || {};
+        this._super(parent, options);
+    },
+    renderElement: function() {
+        var self = this;
+        this._super();
+        this.$el.click(function(){
+            self.ts_model.flush();
+        });
+    },
+    start: function(){
+        var self = this;
+        this.ts_model.bind('change:nbr_pending_operations', function(){
+            self.renderElement();
+        });
+    },
+    get_nbr_pending: function(){
+        return this.ts_model.get('nbr_pending_operations');
+    },
+});
+
+//Generic Buttons whitch actions are part of TsWidget, like close button
+var HeaderButtonWidget = TsBaseWidget.extend({
+    template: 'HeaderButtonWidget',
+    init: function(parent, options){
+        options = options || {};
+        this._super(parent, options);
+        this.action = options.action;
+        this.label   = options.label;
+
+    },
+    renderElement: function(){
+        var self = this;
+        this._super();
+        if(this.action){
+            this.$el.click(function(){ self.action(); });
+        }
+    },
+    show: function(){ this.$el.show(); },
+    hide: function(){ this.$el.hide(); },
+});
+
+var ButtonBlockWidget = TsBaseWidget.extend({
+    init: function(parent,options){
+        this._super(parent,options);
+        this.hidden = false;
+    },
+    show: function(){
+        var self = this;
+        this.hidden = false;
+        if(this.$el){
+            this.$el.show();
+        }
+    },
+    hide: function(){
+        this.hidden = true;
+        if (this.$el){
+            this.$el.hide();
+        }
+    },
+    renderElement: function(){
+        // we need this because some screens re-render themselves when they are hidden
+        // (due to some events, or magic, or both...)  we must make sure they remain hidden.
+        this._super();
+        if(this.hidden){
+            if(this.$el){
+                this.$el.hide();
+            }
+        }
+    },
+    select_screen: function(screen_name){
+        var self = this;
+        this.ts_widget.screen_selector.set_current_screen(screen_name);
+    },
+    select_button_block: function(block_name){
+        var self = this;
+        this.ts_widget.button_block_selector.set_current_block(block_name);
+        // if (this.$('.selected-screen'))
+        //      this.$('.selected-screen').removeClass('selected-screen');
+        // this.$el.addClass('selected-screen');
+    },
+    // close: function(){
+
+    // },
+});
+
+// Buttons block to change between principal screens
+var ScreenButtonWidget = ButtonBlockWidget.extend({
+    template: 'ScreenButtonWidget',
+    init: function(parent,options){
+        this._super(parent,options);
+        this.button_no = _t("New Order");
+        this.button_so = _t("My Orders");
+        this.button_pc = _t("Product Catalog");
+        this.button_pr = _t("Product Reserved");
+        this.button_cl = _t("Call List");
+        this.button_oh = _t("Order History");
+        this.button_ks = _t("Key Shorts");
+    },
+    renderElement: function(){
+        var self = this;
+        this._super();
+        this.$el.find('button#button_no').click(function(){ self.select_screen('new_order');
+                                                          // self.select_button_block('order_buttons');
+                                                          self.setButtonSelected('button#button_no');
+                                                         });
+        this.$el.find('button#button_so').click(function(){ self.select_screen('summary_order');
+                                                          self.setButtonSelected('button#button_so');
+                                                             });
+        this.$el.find('button#button_cl').click(function(){ self.select_screen('call_list');
+                                                          self.setButtonSelected('button#button_cl');
+                                                             });
+        this.$el.find('button#button_oh').click(function(){ self.select_screen('order_history')
+                                                          self.setButtonSelected('button#button_oh');
+                                                             });
+        this.$el.find('button#button_pc').click(function(){ self.select_screen('product_catalog');
+                                                            var upd = self.ts_model.get('update_catalog')
+                                                            if (upd === 'a'){
+                                                                upd = 'b'
+                                                            }
+                                                            else{
+                                                                upd = 'a'
+                                                            }
+                                                            self.ts_model.set('update_catalog', upd)
+                                                          self.setButtonSelected('button#button_pc');
+                                                         });
+        this.$el.find('button#button_pr').click(function(){ self.select_screen('product_reserved');
+                                                           self.setButtonSelected('button#button_pr');
+                                                         });
+        this.$el.find('button#button_ks').click(function(){ self.select_screen('key_shorts');
+                                                           self.setButtonSelected('button#button_ks');
+                                                         });
+
+
+    },
+    setButtonSelected: function(button_selector) {
+        $('.selected-screen').removeClass('selected-screen');
+        $(button_selector).addClass('selected-screen');
+        $('.tab1').focus();
+    },
+});
 
 var TsWidget = TsBaseWidget.extend({
     template: 'TsWidget',
@@ -77,16 +200,16 @@ var TsWidget = TsBaseWidget.extend({
         this.notification.replace(this.$('#placeholder-session-buttons1'));
 
         // Close button --> Close the session
-        // this.close_button = new module.HeaderButtonWidget(this,{
-        //     label: _t('Close'),
-        //     action: function(){ self.try_close(); },
-        // });
-        // this.close_button.replace(this.$('#placeholder-session-buttons2'));
+        this.close_button = new HeaderButtonWidget(this,{
+            label: _t('Close'),
+            action: function(){ self.try_close(); },
+        });
+        this.close_button.replace(this.$('#placeholder-session-buttons2'));
 
         // Buttons navigation screens
-        // this.screen_buttons = new module.ScreenButtonWidget(this, {});
-        // this.screen_buttons.replace(this.$('#placeholder-screen-buttons'));
-        // var self = this;
+        this.screen_buttons = new ScreenButtonWidget(this, {});
+        this.screen_buttons.replace(this.$('#placeholder-screen-buttons'));
+        var self = this;
     },
     loading_progress: function(fac){
             this.$('.loader .loader-feedback').removeClass('oe_hidden');
