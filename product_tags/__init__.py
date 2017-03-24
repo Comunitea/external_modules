363bcaceb9fa687ab8646012473f16305042a850
Omar <omarcs7r@gmail.com>
2017-03-24 15:02:48 +0100

    [ADD] product_tags: Etiquetas de producto

---
+++ product_tags/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013 Julius Network Solutions SARL <contact@julius.fr>
+# Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
