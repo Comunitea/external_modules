e4ab97ba217b92d1030bf526d4c6bfac614f2271
javierjcf <javierjcf@gmail.com>
2017-04-07 14:38:55 +0200

    [ADD]chained_discount_commercial_rules: Descuentos encadenados de la forma x+y+z

---
+++ chained_discount_commercial_rules/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © Openlabs Technologies & Consulting (P) Limited
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Chained discount in commercial Rules',
+    'version': '10.0.0.0.1',
+    'author': 'Comunitea',
+    'website': 'http://www.comunitea.com',
+    'category': 'Generic Modules/Sales & Purchases',
+    'depends': ['commercial_rules'],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/invoice_view.xml',
+    ],
+    'installable': True,
+}
