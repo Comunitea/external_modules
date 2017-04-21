535b10d3512c926d466c913eb823568eb5e73532
javierjcf <javierjcf@gmail.com>
2017-04-21 13:06:14 +0200

    [ADD]telesale: Mostrar clientes

--- telesale/static/src/js/db.js
+++ telesale/static/src/js/db.js
@@ -18,15 +18,20 @@ exports.TS_LS = core.Class.extend({
         //cache the data in memory to avoid roundtrips to the localstorage
         this.cache = {};
 
+        //PRODUCTS DB
         this.product_by_id = {};
         this.product_by_tmp_id = {};
         this.product_code_id = {};
         this.product_name_id = {};
 
+        // PARTNERS DB
+        this.partner_sorted = [];
         this.partner_by_id = {};
         this.partner_ref_id = {};
-        this.partner_search_string = "";
         this.partner_name_id = {};
+        this.partner_search_string = "";
+        this.partner_write_date = null;
+
         this.suppliers_name_id = {};
         this.supplier_from_name_to_id = {};
 
@@ -68,14 +73,6 @@ exports.TS_LS = core.Class.extend({
         }
         return str + '\n';
     },
-    _partner_search_string: function(partner){
-        var str = partner.name;
-        if (partner.ref){
-            str += '|' + partner.ref;
-        }
-        str = '' + partner.id + ':' + str.replace(':','') + '\n';
-        return str
-    },
     add_products: function(products){
         if(!products instanceof Array){
             products = [products];
@@ -102,25 +99,6 @@ exports.TS_LS = core.Class.extend({
             this.unit_name_id[unit.name] = unit.id;
         }
         this.all_units = units
-    },
-    add_partners: function(partners){
-        if(!partners instanceof Array){
-            partners = [partners];
-        }
-        for(var i = 0, len = partners.length; i < len; i++){
-            var partner = partners[i];
-
-            this.partner_by_id[partner.id] = partner;
-            // var cus_name = partner.comercial || partner.name
-            var cus_name = partner.name + ' | ' + partner.ref
-            // var cus_name = this.ts_model.getComplexName(partner)
-            this.partner_name_id[cus_name] = partner.id;
-            if(partner.ref){
-                this.partner_ref_id[partner.ref] = partner.id;
-            }
-            var search_string = this._partner_search_string(partner);
-            this.partner_search_string += search_string
-        }
     },
     add_taxes: function(taxes){
         if(!taxes instanceof Array){
@@ -168,20 +146,119 @@ exports.TS_LS = core.Class.extend({
     get_product_by_id: function(id){
         return this.product_by_id[id];
     },
-    get_product_by_tmp_id: function(id){
-        return this.product_by_tmp_id[id];
-    },
+    // get_product_by_tmp_id: function(id){
+    //     return this.product_by_tmp_id[id];
+    // },
     get_product_by_code: function(code){
         if(this.product_by_code[code]){
             return this.product_by_code[code];
         }
         return undefined;
     },
+    _partner_search_string: function(partner){
+        var str = partner.name;
+        if (partner.ref){
+            str += '|' + partner.ref;
+        }
+        if(partner.address){
+            str += '|' + partner.adress;
+        }
+        if(partner.phone){
+            str += '|' + partner.phone.split(' ').join('');
+        }
+        if(partner.email){
+            str += '|' + partner.email;
+        }
+        if(partner.state_id){
+            str += '|' + partner.email;
+        }
+        str = '' + partner.id + ':' + str.replace(':','') + '\n';
+        return str
+    },
+    // add_partners: function(partners){
+    //     if(!partners instanceof Array){
+    //         partners = [partners];
+    //     }
+    //     for(var i = 0, len = partners.length; i < len; i++){
+    //         var partner = partners[i];
+
+    //         this.partner_by_id[partner.id] = partner;
+    //         var cus_name = partner.name + ' | ' + partner.ref
+    //         this.partner_name_id[cus_name] = partner.id;
+    //         if(partner.ref){
+    //             this.partner_ref_id[partner.ref] = partner.id;
+    //         }
+    //         var search_string = this._partner_search_string(partner);
+    //         this.partner_search_string += search_string
+    //     }
+    // },
+    get_partners_stored: function(max_count){
+        max_count = max_count ? Math.min(this.partner_sorted.length, max_count) : this.partner_sorted.length;
+        var partners = [];
+        for (var i = 0; i < max_count; i++) {
+            partners.push(this.partner_by_id[this.partner_sorted[i]]);
+        }
+        return partners;
+    },
+    add_partners: function(partners){
+        var updated_count = 0;
+        var new_write_date = '';
+        var partner;
+        for(var i = 0, len = partners.length; i < len; i++){
+            partner = partners[i];
+
+            if (    this.partner_write_date && 
+                    this.partner_by_id[partner.id] &&
+                    new Date(this.partner_write_date).getTime() + 1000 >=
+                    new Date(partner.write_date).getTime() ) {
+                // FIXME: The write_date is stored with milisec precision in the database
+                // but the dates we get back are only precise to the second. This means when
+                // you read partners modified strictly after time X, you get back partners that were
+                // modified X - 1 sec ago. 
+                continue;
+            } else if ( new_write_date < partner.write_date ) { 
+                new_write_date  = partner.write_date;
+            }
+            if (!this.partner_by_id[partner.id]) {
+                this.partner_sorted.push(partner.id);
+            }
+            this.partner_by_id[partner.id] = partner;
+
+            updated_count += 1;
+
+            // TODO IMPROVE como obtener el id del partner, partner_name_id no es lo mejor
+            var cus_name = partner.name + ' | ' + partner.ref
+            this.partner_name_id[cus_name] = partner.id;
+            if(partner.ref){
+                this.partner_ref_id[partner.ref] = partner.id;
+            }
+        }
+
+        this.partner_write_date = new_write_date || this.partner_write_date;
+
+        if (updated_count) {
+            // If there were updates, we need to completely 
+            // rebuild the search string and the barcode indexing
+
+            this.partner_search_string = "";
+
+            for (var id in this.partner_by_id) {
+                partner = this.partner_by_id[id];
+
+                partner.address = (partner.street || '') +', '+ 
+                                  (partner.zip || '')    +' '+
+                                  (partner.city || '')   +', '+ 
+                                  (partner.country_id[1] || '');
+                this.partner_search_string += this._partner_search_string(partner);
+            }
+        }
+        return updated_count;
+    },
+    get_partner_write_date: function(){
+        return this.partner_write_date || "1970-01-01 00:00:00";
+    },
     get_partner_by_id: function(id){
         return this.partner_by_id[id];
-    },
-    get_supplier_by_id: function(id){
-        return this.suppliers_name_id[id];
     },
     get_partner_by_code: function(ref){
         if(this.partner_by_ref[ref]){
@@ -191,12 +268,6 @@ exports.TS_LS = core.Class.extend({
     },
     get_unit_by_id: function(id){
         return this.unit_by_id[id];
-    },
-    get_qnote_by_id: function(id){
-        return this.qnote_by_id[id];
-    },
-    get_route_by_id: function(id){
-        return this.route_by_id[id];
     },
     add_order: function(order){
         var last_id = this.load('last_order_id',0);
@@ -214,23 +285,6 @@ exports.TS_LS = core.Class.extend({
     },
     get_orders: function(){
         return this.load('orders',[]);
-    },
-    is_indirect_customer_by_name: function(partner){
-        var res = false;
-        if(partner){
-          var partner_id = this.partner_name_id[partner];
-
-          if (partner_id){
-            var partner_obj = this.get_partner_by_id(partner_id)
-
-            if (partner_obj){
-              res = partner_obj.indirect_customer
-            }
-          }
-
-        }
-
-        return res
     },
 });
 
