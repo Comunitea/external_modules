bd2483be5c6834de09d71f5bcc1c7d84eecbeaae
javierjcf <javierjcf@gmail.com>
2017-04-22 19:54:26 +0200

    [ADD]telesale: Integración con reglas comerciales

--- telesale/static/src/js/order_history_widgets.js
+++ telesale/static/src/js/order_history_widgets.js
@@ -16,46 +16,9 @@ var HistorylineWidget = TsBaseWidget.extend({
         this.open_order = undefined;
         this.order_fetch = undefined;
     },
-    get_order_fields: function(){
-        return ['partner_shipping_id','note','comercial','client_order_ref','name',
-                'partner_id','date_order','state','amount_total','requested_date']
-
-    },
-    get_line_fields: function(){
-        var line_fields = ['product_id','product_uom','product_uom_qty','price_unit',
-                           'tax_id', 'price_subtotal', 'discount'];
-        return line_fields;
-
-    },
-    load_order_from_server: function(order_id, flag){
-        var self=this;
-        if (!flag){
-            this.ts_model.get('orders').add(new models.Order({ ts_model: self.ts_model}));
-        }
-        this.open_order =  this.ts_model.get('selectedOrder')
-        var order_fields = this.get_order_fields();
-        var loaded = self.ts_model.fetch('sale.order', order_fields, [['id', '=', order_id]])
-        .then(function(orders){
-            var order = orders[0];
-            self.order_fetch = order;
-            var line_fields = self.get_line_fields();
-            return self.ts_model.fetch('sale.order.line', line_fields, [['order_id', '=', order_id]]);
-        }).then(function(order_lines){
-            if (flag =='add_lines'){
-                self.open_order.add_lines_to_current_order(order_lines);
-                self.ts_widget.new_order_screen.data_order_widget.refresh();
-                self.ts_widget.new_order_screen.order_widget.change_selected_order()
-                self.ts_widget.new_order_screen.totals_order_widget.changeTotals();
-            }else{
-                self.ts_model.build_order(self.order_fetch, self.open_order, order_lines); //build de order model
-                self.ts_widget.new_order_screen.data_order_widget.refresh();
-            }
-        })
-        return loaded
-    },
     click_handler: function() {
         var self=this;
-        $.when(self.load_order_from_server(self.order.id))
+        $.when(self.ts_widget.new_order_screen.order_widget.load_order_from_server(self.order.id, false))
             .done(function(){
                 $('button#button_no').click();
             });
@@ -67,7 +30,7 @@ var HistorylineWidget = TsBaseWidget.extend({
         if (!partner_id){
                 alert(_t('Please select a customer before adding a order line'));
         }else{
-        $.when(self.load_order_from_server(self.order.id, 'add_lines'))
+        $.when(self.ts_widget.new_order_screen.order_widget.load_order_from_server(self.order.id, 'add_lines'))
             .done(function(){
                 $('button#button_no').click();
             });
