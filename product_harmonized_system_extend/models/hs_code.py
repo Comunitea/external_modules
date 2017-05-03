6fb78251cb20de1bcdf527534af115532dd49248
Omar <omarcs7r@gmail.com>
2017-05-03 18:17:39 +0200

    [ADD] product_harmonized_system_extend: Add a computer url to hs codes

---
+++ product_harmonized_system_extend/models/hs_code.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields, api
+
+
+class HSCode(models.Model):
+
+    _inherit = "hs.code"
+
+    @api.one
+    def _get_url(self):
+        if self.local_code:
+            self.url = (u"http://ec.europa.eu/taxation_customs/dds2/taric/"
+                        u"taric_consultation.jsp?Taric=" + self.local_code +
+                        u"&Expand=true")
+
+    url = fields.Char("Url", compute="_get_url", readonly=True)
