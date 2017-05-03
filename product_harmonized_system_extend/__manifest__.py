ce25c4d1311bf03d1f580b427c5dfeea30d9a3f9
Omar <omarcs7r@gmail.com>
2017-05-03 18:17:39 +0200

    [ADD] product_harmonized_system_extend: Add a computer url to hs codes

---
+++ product_harmonized_system_extend/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Harmonized Systame extend",
+    'version': '10.0.1.0.0',
+    'category': 'Reporting',
+    'author': 'Comunitea',
+    'website': 'http://www.comunitea.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'product_harmonized_system'
+    ],
+    "data": [
+        'views/hs_code_view.xml',
+    ],
+    "installable": True
+}
