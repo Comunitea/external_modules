8f3a13605df4a2ee72ee1191ec66784cd8e92a1c
javierjcf <javierjcf@gmail.com>
2017-05-10 11:25:56 +0200

    [ADD]telesale_manage_variants: No cálculaba el subtotal ni la cantidad al recuperar el pedido

--- telesale_manage_variants/static/src/js/popups.js
+++ telesale_manage_variants/static/src/js/popups.js
@@ -224,6 +224,7 @@ var GridWidget = TsBaseWidget.extend({
         this.$('#add-variants-button').bind('click', function(event){
             self.add_variants_button();
             self.ts_model.ts_widget.new_order_screen.order_widget.renderElement();
+            self.ts_widget.new_order_screen.totals_order_widget.changeTotals();
             self.$('#close-filter').click();
         });
 
