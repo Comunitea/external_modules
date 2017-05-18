315e7858ce112ab7d73564df95ed27ab07155a06
javierjcf <javierjcf@gmail.com>
2017-05-18 18:08:12 +0200

    [ADD]telesale: Nuevos campos a listado de clientes

--- telesale/static/src/js/product_catalog_widgets.js
+++ telesale/static/src/js/product_catalog_widgets.js
@@ -134,6 +134,10 @@ var ProductCatalogWidget = TsBaseWidget.extend({
         }
         return vals
     },
+    //hook
+    get_create_line_vals: function(product_id, catalog_vals, mode){
+        return {}
+    },
     // Get an order line by grid cell cid
     get_line_model_by_cid: function(line_cid){
         var current_order = this.ts_model.get('selectedOrder');
