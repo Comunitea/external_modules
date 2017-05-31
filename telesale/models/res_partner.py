094f72899396ec5723e3514f91bf402dc4e9cb8a
javierjcf <javierjcf@gmail.com>
2017-05-31 13:54:07 +0200

    [ADD]telesale: Método para crear desde televenta un cliente

--- telesale/models/res_partner.py
+++ telesale/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -8,3 +8,12 @@ class ResPartner(models.Model):
 
     commercial_partner_name = fields.Char(string='Commercial Name',
                                           related='commercial_partner_id.name')
+
+    @api.model
+    def update_partner_from_ui(self, partner):
+         partner_id = partner.pop('id', False)
+         if partner_id:  # Modifying existing partner
+             self.browse(partner_id).write(partner)
+         else:
+             partner_id = self.create(partner).id
+         return partner_id
