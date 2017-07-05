4a78c98908f447a7b34168f10765e615ab03d86f
javierjcf <javierjcf@gmail.com>
2017-07-05 19:57:02 +0200

    [ADD]purchase_variant_mgmt: Grid variantes en compras

---
+++ purchase_variant_mgmt/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Purchase Variant Management",
+    'version': '10.0.1.0.0',
+    'category': 'Textile Vertical',
+    'author': 'Comunitea',
+    'website': 'http://www.comunitea.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'purchase',
+        'web_widget_x2many_2d_matrix',
+    ],
+    "data": [
+        'wizard/purchase_manage_variant_view.xml',
+        'views/purchase_view.xml',
+    ],
+    "installable": True
+}
---
+++ purchase_variant_mgmt/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import purchase
