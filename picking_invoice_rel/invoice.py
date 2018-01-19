fae4aae9cf81346a9994132ba86ae71679d7a3f9
Pedro Gomez <pegomez@elnogal.com>
2018-01-19 11:45:20 +0100

    Se impide propagar el valor del campo 'stock_move_id' al duplicar una factura o hacer una rectificativa

--- picking_invoice_rel/invoice.py
+++ picking_invoice_rel/invoice.py
@@ -20,38 +20,53 @@
 #
 ##############################################################################
 from openerp.osv import fields, osv
+from openerp import api
+
 
 class account_invoice(osv.osv):
     _inherit = "account.invoice"
 
 # FIXME -this is not used, because info is in account_invoice.name
     def _client_order_refs(self, cr, uid, ids, field_name, arg, context=None):
-         result = {}
-         for inv in self.browse(cr, uid, ids, context):
+        result = {}
+        for inv in self.browse(cr, uid, ids, context):
             client_ref = '' 
             for ref in inv.sale_order_ids:
-               if ref.client_order_ref:
-                   if client_ref:
-                       client_ref +='; '
-                   client_ref += ref.client_order_ref
+                if ref.client_order_ref:
+                    if client_ref:
+                        client_ref +='; '
+                    client_ref += ref.client_order_ref
             result[inv.id] = client_ref
-         return result
-
+        return result
 
     _columns = {
         'picking_ids': fields.many2many('stock.picking', 'picking_invoice_rel', 'invoice_id', 'picking_id', 'Pickings' ),
         'sale_order_ids': fields.many2many('sale.order', 'sale_order_invoice_rel', 'invoice_id', 'order_id', 'Sale Orders', readonly=True, help="This is the list of sale orders linked to this invoice. "),
         'client_order_refs' : fields.function(_client_order_refs, method=True, string="Client Sale Orders Ref", type='char'),
     }
+
     def copy(self, cr, uid, id, default=None, context=None):
         default = default or {}
         default.update({
             'picking_ids':[],
             'sale_order_ids':[],
             })
-        return super(account_invoice, self).copy(cr, uid, id, default, context)
-                            
+        res = super(account_invoice, self).copy(cr, uid, id, default, context)
+        invoice_id = self.pool.get('account.invoice').browse(cr, uid, [res], context)
+        invoice_id.invoice_line.write({'stock_move_id': False})
+        return res
+
+    @api.model
+    def _refund_cleanup_lines(self, lines):
+        res = super(account_invoice, self)._refund_cleanup_lines(lines)
+        if lines and lines[0]._name != 'account.invoice.line':
+            return res
+        for i, line in enumerate(lines):
+            res[i][2]['stock_move_id'] = False
+        return res
+
 account_invoice()
+
 
 class account_invoice_line(osv.osv):
     _inherit = "account.invoice.line"
