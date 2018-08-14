7b85218330ea3a9358d09835f407e31d85204a1a
javierjcf <javierjcf@gmail.com>
2018-08-14 17:12:54 +0200

    [FIX]account_due_dates_str: api.one por api.multi

--- account_due_dates_str/models/invoice.py
+++ account_due_dates_str/models/invoice.py
@@ -29,30 +29,32 @@ class AccountInvoice(models.Model):
 
     _inherit = "account.invoice"
 
-    @api.one
+    @api.multi
     def _get_move_lines_str(self):
         """returns all move lines related to invoice in string"""
         expiration_dates_str = ""
         move_line_obj = self.env["account.move.line"]
-        if self.move_id:
-            move_lines = move_line_obj.\
-                search([('move_id', '=', self.move_id.id),
-                        ('account_id.internal_type', 'in',
-                         ['payable', 'receivable']),
-                        ('date_maturity', "!=", False)],
-                       order="date_maturity asc")
-            for line in move_lines:
-                date = time.strptime(line.date_maturity, "%Y-%m-%d")
-                date = datetime.fromtimestamp(mktime(date))
-                date = date.strftime("%d/%m/%Y")
-                expiration_dates_str += date + \
-                    " -------------> " + \
-                    (self.type in ('out_invoice', 'in_refund') and
-                     tools.formatLang(self.env, line.debit) or
-                     (self.type in ('in_invoice', 'out_refund') and
-                      tools.formatLang(self.env, line.credit) or "0")) + "\n"
+        for invoice in self:
+            if invoice.move_id:
+                move_lines = move_line_obj.\
+                    search([('move_id', '=', invoice.move_id.id),
+                            ('account_id.internal_type', 'in',
+                             ['payable', 'receivable']),
+                            ('date_maturity', "!=", False)],
+                           order="date_maturity asc")
+                for line in move_lines:
+                    date = time.strptime(line.date_maturity, "%Y-%m-%d")
+                    date = datetime.fromtimestamp(mktime(date))
+                    date = date.strftime("%d/%m/%Y")
+                    expiration_dates_str += date + \
+                        " -------------> " + \
+                        (invoice.type in ('out_invoice', 'in_refund') and
+                         tools.formatLang(invoice.env, line.debit) or
+                         (invoice.type in ('in_invoice', 'out_refund') and
+                          tools.formatLang(invoice.env, line.credit) or
+                          "0")) + "\n"
 
-        self.expiration_dates_str = expiration_dates_str
+            invoice.expiration_dates_str = expiration_dates_str
 
     expiration_dates_str = fields.Text('Expiration dates', readonly=True,
                                        compute='_get_move_lines_str')
