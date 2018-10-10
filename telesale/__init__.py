c8bcee951c7048bf13fe4b952c7233fe12428e7b
javierjcf <javierjcf@gmail.com>
2018-10-10 11:38:18 +0200

    [FIX]telesale: Correciones para que sea instalable.

--- telesale/__init__.py
+++ telesale/__init__.py
@@ -2,5 +2,5 @@
 # © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import models
-import controllers
+from . import models
+from . import controllers
