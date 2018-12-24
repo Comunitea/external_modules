d85c2d54d216fd3a009add29a9df7567765a5bef
neo-oien <i@oien.ru>
2018-12-24 15:04:27 +0100

    [FIX] Checkout coupon - moving to another repository

--- checkout_coupon/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2018 Comunitea - Pavel Smirnov <pavel@comunitea.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import controllers
-from . import models
