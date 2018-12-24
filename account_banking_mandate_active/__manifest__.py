60e7f05a6103c4cd3808d032d3f6247ca9828756
JesusVMayor <jesus@comunitea.com>
2018-12-24 13:42:35 +0100

    [FIX]account_banking_mandate_active,res_partner_bank_active: Migrados a 11.0

--- account_banking_mandate_active/__manifest__.py
+++ account_banking_mandate_active/__manifest__.py
@@ -1,33 +1,16 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2016
-#    Comunitea Servicios Tecnológicos (http://www.comunitea.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name" : "Active Banking mandates",
+    "name": "Active Banking mandates",
     "description": """
        Allows to deactive banking mandates
     """,
-    "version" : "10.0.1.0.0",
-    "author" : "Comunitea",
+    "version": "11.0.1.0.0",
+    "author": "Comunitea",
+    'license': 'AGPL-3',
     "website": "http://www.comunitea.com",
-    "depends" : ["account_banking_mandate", "res_partner_bank_active"],
-    "category" : "Accounting",
-    "data" : ['views/account_banking_mandate_view.xml'],
+    "depends": ["account_banking_mandate", "res_partner_bank_active"],
+    "category": "Accounting",
+    "data": ['views/account_banking_mandate_view.xml'],
     'installable': True,
 }
