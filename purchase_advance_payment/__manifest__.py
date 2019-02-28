28de5b7739acfc747be969f713a345a555f7ef71
Omar (Comunitea) <omar@comunitea.com>
2019-02-28 19:00:07 +0100

    [ADD] purchase_advance_payment,sale_advance_payment: Migrados a la 11.0

---
+++ purchase_advance_payment/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Purchase Advance Payment",
+    "version": "11.0.1.0.0",
+    "author": "Comunitea",
+    'website': 'www.comunitea.com',
+    "category": "Purchases",
+    "description": """Allow to add advance payments on purchases and then use
+ its on invoices""",
+    "depends": ["purchase", "account"],
+    "data": ['wizard/purchase_advance_payment_wzd_view.xml',
+             'views/purchase_view.xml',
+             'security/ir.model.access.csv'],
+    "installable": True,
+}
