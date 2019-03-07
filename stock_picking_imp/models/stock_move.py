d4af5a9afebe2e969a1e4aacb75b7d2c5280834f
Kiko Sánchez <kiko@comunitea.com>
2019-03-07 17:48:04 +0100

    [DEV] stock_picking_imp

---
+++ stock_picking_imp/models/stock_move.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Kiko Sánchez, <kiko@comunitea.com> Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+from datetime import timedelta
+
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+
+    def _get_new_picking_domain(self):
+
+        return [
+                ('group_id', '=', self.group_id.id),
+                ('location_id', '=', self.location_id.id),
+                ('location_dest_id', '=', self.location_dest_id.id),
+                ('picking_type_id', '=', self.picking_type_id.id),
+                ('printed', '=', False),
+                ('state', 'in', ['draft', 'confirmed', 'waiting', 'partially_available', 'assigned'])]
+
+    def _assign_picking(self):
+        """HEREDO TODO PARA SACAR TODO FUERA Y PODER HEREDAR"""
+
+        """ Try to assign the moves to an existing picking that has not been
+        reserved yet and has the same procurement group, locations and picking
+        type (moves should already have them identical). Otherwise, create a new
+        picking to assign them to. """
+
+        Picking = self.env['stock.picking']
+        for move in self:
+            recompute = False
+            print (move._get_new_picking_domain())
+            picking = Picking.search(move._get_new_picking_domain(), limit=1)
+            if picking:
+                if picking.partner_id.id != move.partner_id.id or picking.origin != move.origin:
+                    # If a picking is found, we'll append `move` to its move list and thus its
+                    # `partner_id` and `ref` field will refer to multiple records. In this
+                    # case, we chose to  wipe them.
+                    picking.write({
+                        'partner_id': False,
+                        'origin': False,
+                    })
+            else:
+                recompute = True
+                picking = Picking.create(move._get_new_picking_values())
+            move.write({'picking_id': picking.id})
+            move._assign_picking_post_process(new=recompute)
+            # If this method is called in batch by a write on a one2many and
+            # at some point had to create a picking, some next iterations could
+            # try to find back the created picking. As we look for it by searching
+            # on some computed fields, we have to force a recompute, else the
+            # record won't be found.
+            if recompute:
+                move.recompute()
+        return True
