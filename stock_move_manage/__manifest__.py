72feff49c42331d49e69cfa740ef60714735ee77
javierjcf <javierjcf@gmail.com>
2019-03-08 12:36:30 +0100

    [FIX]stock_move_mage: Cambiado nombre

--- stock_move_manage/__manifest__.py
+++ stock_move_manage/__manifest__.py
@@ -2,7 +2,7 @@
 # © 2018 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    'name': 'Stock Move Custom Anzamar',
+    'name': 'Stock Move Manage',
     'version': '11.0.0.0.0',
     'category': 'Custom',
     'license': 'AGPL-3',
