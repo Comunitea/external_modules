4f7c5ee15db32886151a06573bb629be34f280b2
Kiko Sánchez <kiko@comunitea.com>
2019-03-21 10:26:55 +0100

    [IMP] delivery_packing_list Se añade funcionalidad sscc para etiquetaje de palets

--- delivery_packing_list/models/__init__.py
+++ delivery_packing_list/models/__init__.py
@@ -1,3 +1,3 @@
 # © 2018 Comunitea - Kiko Sánchez <kiko@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import stock_picking,stock_packing_list
+from . import stock_picking,stock_packing_list,res_company
