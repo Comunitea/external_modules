7ee7a4d66fcf8aab4450b3799f3dc257d325562c
Roodin <santi@comunitea.com>
2019-04-22 12:14:17 +0200

    [ADD] account_invoice_move_cost

---
+++ account_invoice_move_cost/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2013-2014 Odoo SA
+# Copyright 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Purchase Invoice Move Cost',
+    'summary': 'Purchase Invoice Move Cost',
+    'version': '11.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'website': 'https://comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'installable': True,
+}
