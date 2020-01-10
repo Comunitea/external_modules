8e4bb3d7e0c2d413365a089efe592aefdbe9dcf6
Omar (Comunitea) <omar@comunitea.com>
2020-01-10 16:23:05 +0100

    [ADD] web_responsive_search_allowed_menu_technical_features: Compatibiliza web_responsive_search_allowed_menu y base_technical_features

---
+++ web_responsive_search_allowed_menu_technical_features/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Allow search only visible menus web_responsive "
+            "with technial_features",
+    "version": "11.0.0.0.0",
+    "author": "Comunitea",
+    "website": "https://www.comunitea.com",
+    "category": "Website",
+    "summary": "Force the correct load of addons base_technical_features and "
+               "web_responsive_search_allowed_menu",
+    "depends": ['web_responsive_search_allowed_menu',
+                'base_technical_features'],
+    "data": [],
+    "installable": True,
+    'license': 'AGPL-3',
+    "auto_install": True
+}
