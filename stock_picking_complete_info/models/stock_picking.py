4997e445961b3dff3f0a854267db0eae2a82eed2
Kiko Sánchez <kiko@comunitea.com>
2020-01-12 11:57:51 +0100

    [IMP] stock_picking_complete_info. Se añade el campo all_assigned para distinguier preparado de 100% preparado

--- stock_picking_complete_info/models/stock_picking.py
+++ stock_picking_complete_info/models/stock_picking.py
@@ -33,40 +33,90 @@ class StockPicking(models.Model):
         digits=dp.get_precision('Product Unit of Measure'))
     product_ids_count = fields.Integer('# Products',
                                        compute='_count_product_ids')
-    all_assigned = fields.Boolean('All assigned', compute='get_all_assigned')
+    all_assigned = fields.Boolean('All assigned', compute='_compute_state', store=True, copy=False)
     currency_id = fields.Many2one(related='sale_id.currency_id')
-    price_subtotal = fields.Monetary(string='Subtotal', currency_field='currency_id', compute='get_n_lines',store=True)
+    price_subtotal = fields.Monetary(string='Subtotal', currency_field='currency_id', compute='get_n_lines', store=True)
     move_lines_count = fields.Integer ('# Lines', compute='get_n_lines',store=True)
     info_str = fields.Char('Info str', compute='get_n_lines',store=True)
     n_lines = fields.Char(store=False)
     n_amount = fields.Char(store=False)
 
+    @api.depends('move_type', 'move_lines.state', 'move_lines.picking_id')
+    @api.one
+    def _compute_state(self):
+
+        '''
+        TODO SOBREESCRIBO TODO PQ NO HE ENCONTRADO OTRA HERENCIA, SI LA HUBIERA
+        super()._compute_state()
+        if self.move_lines._get_relevant_state_among_moves() in ('assigned', 'done'):
+            self.all_assigned = True
+        else:
+          self.all_assigned = False
+        todo ..................................................................
+        '''
+
+        ''' State of a picking depends on the state of its related stock.move
+        - Draft: only used for "planned pickings"
+        - Waiting: if the picking is not ready to be sent so if
+          - (a) no quantity could be reserved at all or if
+          - (b) some quantities could be reserved and the shipping policy is "deliver all at once"
+        - Waiting another move: if the picking is waiting for another move
+        - Ready: if the picking is ready to be sent so if:
+          - (a) all quantities are reserved or if
+          - (b) some quantities could be reserved and the shipping policy is "as soon as possible"
+        - Done: if the picking is done.
+        - Cancelled: if the picking is cancelled
+        '''
+
+        self.all_assigned = False
+        if not self.move_lines:
+            self.state = 'draft'
+        elif any(move.state == 'draft' for move in self.move_lines):  # TDE FIXME: should be all ?
+            self.state = 'draft'
+        elif all(move.state == 'cancel' for move in self.move_lines):
+            self.state = 'cancel'
+        elif all(move.state in ['cancel', 'done'] for move in self.move_lines):
+            self.state = 'done'
+        else:
+            relevant_move_state = self.move_lines._get_relevant_state_among_moves()
+            if relevant_move_state in ('assigned', 'done'):
+                self.all_assigned = True
+            if relevant_move_state == 'partially_available':
+                self.state = 'assigned'
+            else:
+                self.state = relevant_move_state
+
+
     @api.multi
     @api.depends('move_lines', 'state')
     def get_n_lines(self):
         for pick in self:
+            if pick.picking_type_id.code == 'outgoing':
+                lines_for_total = pick.move_lines
+            else:
+                lines_for_total = pick.move_lines.mapped('move_dest_ids').filtered(lambda x: x.picking_type_id.code == 'outgoing')
             if pick.state == 'done':
                 move_lines_count_not = pick.move_lines_count = len(pick.move_lines)
-                pick.price_subtotal = sum(x.price_subtotal for x in pick.move_lines.filtered(
+                pick.price_subtotal = sum(x.price_subtotal for x in lines_for_total.filtered(
                     lambda x: x.state == 'done'))
                 state_text = 'Done'
             elif pick.state == 'assigned':
                 pick.move_lines_count = len(pick.move_lines)
                 move_lines_count_not = len(pick.move_lines.filtered(lambda x: x.reserved_availability))
-                pick.price_subtotal = sum(x.price_subtotal for x in pick.move_lines.filtered(
+                pick.price_subtotal = sum(x.price_subtotal for x in lines_for_total.filtered(
                     lambda x: x.state in ('partially_available', 'assigned')))
                 state_text = 'Assigned'
             else:
                 state_text = 'To assign'
-                pick.price_subtotal = sum(x.price_subtotal for x in pick.move_lines.filtered(
+                pick.price_subtotal = sum(x.price_subtotal for x in lines_for_total.filtered(
                     lambda x: x.state not in ('draft', 'cancel')))
                 move_lines_count_not = pick.move_lines_count = len(pick.move_lines)
             pick.info_str = _('{} €: {} {} of {} lines'.format(pick.price_subtotal, state_text, move_lines_count_not, pick.move_lines_count))
 
     @api.multi
-    def get_all_assigned(self):
-        for pick in self:
-            pick.all_assigned = not any(x.state in ('partially_available', 'confirmed') for x in pick.move_lines)
+    def unlink_from_batch(self):
+        self.write({'batch_id': False})
+
 
     @api.multi
     def compute_picking_qties(self):
@@ -104,7 +154,6 @@ class StockPicking(models.Model):
 
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
-        print (args)
         if self._context.get('n_amount') == True:
             for arg in args:
                 if arg[0] == 'n_amount':
@@ -124,8 +173,7 @@ class StockPicking(models.Model):
                         except:
                             pass
                     args.remove(arg)
-
-
+                    break
         if self._context.get('n_lines') == True:
             for arg in args:
                 if arg[0] == 'n_lines':
@@ -145,5 +193,5 @@ class StockPicking(models.Model):
                         except:
                             pass
                     args.remove(arg)
-
+                    break
         return super().search(args, offset, limit, order, count)
