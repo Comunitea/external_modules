e70f8ec6e808f8fd816870ea406c6f801da4596e
Omar (Comunitea) <omar@comunitea.com>
2020-02-05 16:30:44 +0100

    [ADD] sale_order_invoice_policy: Permite cambiar la política de facturación para una venta en concreto

---
+++ sale_order_invoice_policy/__manifest__.py
@@ -0,0 +1,19 @@
+# Learn more or give us feedback
+# Copyright 2020 Omar Castiñeira Saavedra <omar@comunitea.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Sale Order Invoice Policy",
+    "version": "11.0.1.0.0",
+    "category": "Sales Management",
+    "author": "Comunitea",
+    "website": "https://comunitea.com",
+    "license": "AGPL-3",
+    "depends": [
+        'sale',
+    ],
+    "data": [
+        'views/sale_order.xml',
+    ],
+    'installable': True,
+}
