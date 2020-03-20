d60b7cd11a03565092adf41eb5bb96a949b86a63
JesusVMayor <jesus@comunitea.com>
2020-03-20 16:07:34 +0100

    [ADD]rma_portal: Añadido módulo.

---
+++ rma_portal/__manifest__.py
@@ -0,0 +1,14 @@
+# © 2020 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "RMA portal",
+    "version": "12.0.1.0.0",
+    "category": "RMA",
+    "author": "Comunitea",
+    "maintainer": "Comunitea",
+    "website": "www.comunitea.com",
+    "license": "AGPL-3",
+    "contributors": [],
+    "depends": ["portal", "rma"],
+    "data": ['views/rma_portal_templates.xml', 'security/rma_security.xml', 'security/ir.model.access.csv'],
+}
