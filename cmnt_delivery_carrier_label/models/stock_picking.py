04d443a37d4d097a872e26cca143156862df74f2
JesusVMayor <jesus@comunitea.com>
2020-04-24 11:00:32 +0200

    [IMP]cmnt_delivery_carrier_label: Botón para reimprimir etiquetas

--- cmnt_delivery_carrier_label/models/stock_picking.py
+++ cmnt_delivery_carrier_label/models/stock_picking.py
@@ -1,6 +1,8 @@
 # © 2020 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, fields, models
+from odoo.exceptions import UserError
+from base64 import b64decode
 
 
 class StockPicking(models.Model):
@@ -14,3 +16,16 @@ class StockPicking(models.Model):
     carrier_weight = fields.Float()
     carrier_packages = fields.Integer(default=1)
     carrier_service = fields.Many2one('delivery.carrier.service')
+
+    def print_created_labels(self):
+        self.ensure_one()
+
+        if not self.carrier_id.account_id.printer:
+            raise UserError('Printer not defined')
+        labels = self.env['shipping.label'].search([
+            ('res_id', '=', self.id),
+            ('res_model', '=', 'stock.picking')])
+        for label in labels:
+            self.carrier_id.account_id.printer.print_document(
+                None, b64decode(label.datas), doc_format="raw"
+            )
