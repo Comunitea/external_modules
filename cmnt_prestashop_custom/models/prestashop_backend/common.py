2789d1d0f6ed04f46f936fe20b9e35ae9721a022
JesusVMayor <jesus@comunitea.com>
2020-05-04 10:34:17 +0200

    [ADD]cmnt_prestashop:custom: Añadidos desarrollos comunes a proyectos.

---
+++ cmnt_prestashop_custom/models/prestashop_backend/common.py
@@ -0,0 +1,12 @@
+# © 2020 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class PrestashopBackend(models.Model):
+    _inherit = "prestashop.backend"
+
+    import_image_type = fields.Selection(
+        [("url", "URL"), ("db", "Database")], default="url"
+    )
+    resize_images = fields.Boolean()
