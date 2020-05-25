16a26b3e56126a00890b6c47c84364e23b68b07b
JesusVMayor <jesus@comunitea.com>
2020-05-25 10:18:25 +0200

    [IMP]Añadida cancelación de pedidos de prestashop.

--- cmnt_prestashop_custom/models/sale_order/common.py
+++ cmnt_prestashop_custom/models/sale_order/common.py
@@ -8,3 +8,17 @@ class SaleOrde(models.Model):
     _inherit = "sale.order"
 
     prestashop_state = fields.Many2one("sale.order.state")
+
+    def write(self, vals):
+        res = super().write(vals)
+        for order in self:
+            if vals.get("prestashop_state"):
+                state = order.prestashop_state
+                if state.trigger_cancel:
+                    order.invoice_ids.filtered(
+                        lambda r: r.state == "draft"
+                    ).action_cancel()
+                    if order.state == "done":
+                        order.action_unlock()
+                    order.action_cancel()
+        return res
