c85efa43e237b8b76f5b5b045287fad5fc5b339d
JesusVMayor <jesus@comunitea.com>
2020-05-29 15:27:10 +0200

    [IMP]rma_warranty: Mejoras en gestión de garantía. Se configura un periodo mínimo de garantía. Si el número de serie tiene una garantía inferior al hacer la salida se actualiza a la duración mínima

--- rma_warranty/models/stock_move_line.py
+++ rma_warranty/models/stock_move_line.py
@@ -14,10 +14,29 @@ class StockMoveLine(models.Model):
             lambda r: r.picking_id.picking_type_id.code == "outgoing"
         ):
             if move_line.lot_id and move_line.product_id.warranty_duration:
-                warranty_termination = move_line.date + relativedelta(
-                    days=move_line.product_id.warranty_duration
-                )
-                move_line.lot_id.warranty_termination = (
-                    warranty_termination.date()
-                )
+                if (
+                    move_line.lot_id.warranty_termination
+                    and move_line.move_id.rma_line_id
+                ):
+                    config_parameter = self.env['ir.config_parameter']
+                    min_warranty = int(config_parameter.get_param(
+                        'rma.minimum_warranty', default=0))
+                    if (
+                        move_line.lot_id.warranty_termination
+                        < move_line.date.date() + relativedelta(days=min_warranty)
+                    ):
+                        warranty_termination = move_line.date + relativedelta(
+                            days=min_warranty
+                        )
+                        move_line.lot_id.warranty_termination = (
+                            warranty_termination.date()
+                        )
+
+                else:
+                    warranty_termination = move_line.date + relativedelta(
+                        days=move_line.product_id.warranty_duration
+                    )
+                    move_line.lot_id.warranty_termination = (
+                        warranty_termination.date()
+                    )
         return res
