41c6f25c941d54e0bbb68a4ee5f36c31e3d87a35
JesusVMayor <jesus@comunitea.com>
2020-06-30 00:49:03 +0200

    [IMP]cmnt_prestashop_custom: Mapeo de provincias portuguesas.

--- cmnt_prestashop_custom/models/res_partner/importer.py
+++ cmnt_prestashop_custom/models/res_partner/importer.py
@@ -18,5 +18,21 @@ class PartnerImportMapper(Component):
                     ("city_id.country_id", "=", country.id),
                 ], limit=1
             )
+            if not city_zip:
+                # Portugal
+                city_zip = self.env["res.city.zip"].search(
+                    [
+                        ("name", "=", record.get("postcode").replace(' ', '-')),
+                        ("city_id.country_id", "=", country.id),
+                    ], limit=1
+                )
+                if not city_zip:
+                    # Portugal 2
+                    city_zip = self.env["res.city.zip"].search(
+                        [
+                            ("name", "=", record.get("postcode")[:4] + '-' + record.get("postcode")[4:]),
+                            ("city_id.country_id", "=", country.id),
+                        ], limit=1
+                    )
             if city_zip:
                 return {"state_id": city_zip.city_id.state_id.id}
