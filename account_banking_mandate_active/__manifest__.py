b4cbe2d05841d320ec4ff05f53f9195fc5f554c9
Omar (Comunitea) <omar@comunitea.com>
2021-02-22 16:42:37 +0100

    [MIG] account_banking_mandate_active: Móudlo migrado a las 12.0

---
+++ account_banking_mandate_active/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Active Banking mandates",
+    "description": """
+       Allows to deactive banking mandates
+    """,
+    "version": "12.0.1.0.0",
+    "author": "Comunitea",
+    'license': 'AGPL-3',
+    "website": "https://www.comunitea.com",
+    "depends": ["account_banking_mandate", "partner_bank_active"],
+    "category": "Accounting",
+    "data": ['views/account_banking_mandate_view.xml'],
+    'installable': True,
+}
