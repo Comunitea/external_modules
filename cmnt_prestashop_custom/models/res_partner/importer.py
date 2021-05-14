dad4b225392da0b5b3e14f3682f6746ff2c6b905
JesusVMayor <jesus@comunitea.com>
2021-05-14 11:37:57 +0200

    [FIX]cmnt_prestashop_custom: Importación de provincia en direcciones.

--- cmnt_prestashop_custom/models/res_partner/importer.py
+++ cmnt_prestashop_custom/models/res_partner/importer.py
@@ -36,3 +36,37 @@ class PartnerImportMapper(Component):
                     )
             if city_zip:
                 return {"state_id": city_zip.city_id.state_id.id}
+
+
+class AddressImportMapper(Component):
+    _inherit = "prestashop.address.mappper"
+
+    @mapping
+    def state_id(self, record):
+        if record.get("id_country") and record.get("postcode"):
+            binder = self.binder_for("prestashop.res.country")
+            country = binder.to_internal(record["id_country"], unwrap=True)
+            city_zip = self.env["res.city.zip"].search(
+                [
+                    ("name", "=", record.get("postcode")),
+                    ("city_id.country_id", "=", country.id),
+                ], limit=1
+            )
+            if not city_zip:
+                # Portugal
+                city_zip = self.env["res.city.zip"].search(
+                    [
+                        ("name", "=", record.get("postcode").replace(' ', '-')),
+                        ("city_id.country_id", "=", country.id),
+                    ], limit=1
+                )
+                if not city_zip:
+                    # Portugal 2
+                    city_zip = self.env["res.city.zip"].search(
+                        [
+                            ("name", "=", record.get("postcode")[:4] + '-' + record.get("postcode")[4:]),
+                            ("city_id.country_id", "=", country.id),
+                        ], limit=1
+                    )
+            if city_zip:
+                return {"state_id": city_zip.city_id.state_id.id}
