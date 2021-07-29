ac9dc34b16807464ee6f61ecc9662623fe1b1e7f
Omar (Comunitea) <omar@comunitea.com>
2021-07-29 20:20:51 +0200

    [MIG] product_harmonized_system_extend: Migrado a 14.0

--- product_harmonized_system_extend/__manifest__.py
+++ product_harmonized_system_extend/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': "Harmonized Systame extend",
-    'version': '10.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Reporting',
     'author': 'Comunitea',
-    'website': 'http://www.comunitea.com',
+    'website': 'https://www.comunitea.com',
     'license': 'AGPL-3',
     "depends": [
         'product_harmonized_system'
--- product_harmonized_system_extend/models/hs_code.py
+++ product_harmonized_system_extend/models/hs_code.py
@@ -1,20 +1,17 @@
-# -*- coding: utf-8 -*-
 # © 2017 Comunitea Servicios Tecnológicos S.L. (http://comunitea.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class HSCode(models.Model):
 
     _inherit = "hs.code"
 
-    @api.one
-    @api.depends('local_code')
     def _get_url(self):
         if self.local_code:
-            self.url = (u"http://ec.europa.eu/taxation_customs/dds2/taric/"
-                        u"taric_consultation.jsp?Taric=" + self.local_code +
-                        u"&Expand=true")
+            self.url = ("http://ec.europa.eu/taxation_customs/dds2/taric/"
+                        "taric_consultation.jsp?Taric=" + self.local_code +
+                        "&Expand=true")
 
     url = fields.Char("Url", compute="_get_url", readonly=True)
