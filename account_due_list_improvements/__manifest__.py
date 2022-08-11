b728e11fd4bc405b0321a86e2857406d37cd7448
Miguel Vazquez <mendezmiguel052202@gmail.com>
2022-08-11 10:43:03 +0200

    [ADD] account_due_list_improvements

---
+++ account_due_list_improvements/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Account Due List Improvements",
+    "version": "13.0.1.0.0",
+    "author": "Comunitea",
+    "website": "https://www.comunitea.com",
+    "description": "Account Due List Improvements",
+    "depends": ['account_payment_sale', 'account_due_list'],
+    "data": ['views/account_move.xml'],
+    "installable": True,
+    'license': 'AGPL-3'
+}
