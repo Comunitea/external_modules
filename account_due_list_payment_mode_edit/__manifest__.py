f5b82a8c0bc284ae377a34549fe546496165f4b6
David Alonso // Solvos <david.alonso@solvos.es>
2022-10-06 18:30:09 +0200

    [IMP] account_due_list_payment_mode_edit: modo de pago seleccionable según el tipo de efecto

--- account_due_list_payment_mode_edit/__manifest__.py
+++ account_due_list_payment_mode_edit/__manifest__.py
@@ -1,11 +1,14 @@
 {
     "name": "Account Due List Payment mode editable",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "author": "Comunitea",
     "website": "https://www.comunitea.com",
     "description": "Account Due List Improvements",
     "depends": ['account_due_list_payment_mode'],
-    "data": ['views/account_move.xml'],
+    "data": [
+        'views/account_move.xml',
+        'views/account_move_line.xml',
+    ],
     "installable": True,
     'license': 'AGPL-3'
 }
--- account_due_list_payment_mode_edit/models/account_move_line.py
+++ account_due_list_payment_mode_edit/models/account_move_line.py
@@ -1,5 +1,9 @@
 from odoo import fields, models
 
+PAYMENT_MODE_MAPPING = {
+    "payable": "outbound",
+    "receivable": "inbound",
+}
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
@@ -7,3 +11,17 @@ class AccountMoveLine(models.Model):
     payment_mode_id = fields.Many2one(
         readonly=False,
     )
+
+    payment_mode_type = fields.Char(
+        compute="_compute_payment_mode_type",
+        help="""
+        Technical field that enables payment mode restricted selection
+        depending on due type (payable or receivable)
+        """,
+    )
+
+    def _compute_payment_mode_type(self):
+        for move_line in self:
+            move_line.payment_mode_type = PAYMENT_MODE_MAPPING.get(
+                move_line.account_internal_type, "other"
+            )
