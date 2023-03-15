9a82454da9d98d432d31ff02eb94dc0a5045c923
javierjcf <javier@comunitea.com>
2023-03-15 17:17:27 +0100

    Eliminado módulo de asec

--- payment_return_invoice_resend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# © 2019 Comunitea
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import account_invoice
-from . import payment_return
-from . import company
-from . import account_journal
