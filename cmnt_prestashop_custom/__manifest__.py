ba2a9d4f03f9ef37fd432802c2b1915b23809797
javierjcf <javier@comunitea.com>
2025-05-13 10:20:24 +0200

    [MIG]Odoo Module Migratos de v16 a v17

--- cmnt_prestashop_custom/__manifest__.py
+++ cmnt_prestashop_custom/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Prestashop customizations from Comunitea',
-    'version': '16.0.1.0.0',
+    'version': "17.0.1.0.0",
     'category': 'Connector',
     'author': 'Comunitea',
     'maintainer': 'Comunitea',
