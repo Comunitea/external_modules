ad2d98a9dc443b73ff20ea57f3ebf565941dc663
javierjcf <javier@comunitea.com>
2025-06-18 13:50:13 +0200

    [MIG]cmnt_prestashop_custom: Migrate to v18

--- cmnt_prestashop_custom/__manifest__.py
+++ cmnt_prestashop_custom/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Prestashop customizations from Comunitea',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Connector',
     'author': 'Comunitea',
     'maintainer': 'Comunitea',
