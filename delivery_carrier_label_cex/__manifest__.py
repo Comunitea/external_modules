303372e505f9f67176bfaa86967a76a1b36136ba
Kiko <kiko@comunitea.com>
2025-09-15 11:39:02 +0200

    Update __manifest__.py
    Error in versión

--- delivery_carrier_label_cex/__manifest__.py
+++ delivery_carrier_label_cex/__manifest__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Correos Express',
-    'version': '12.0.1.0.0',
+    'version': '18.0.1.0',
     'category': 'Delivery',
     'license': 'AGPL-3',
     'summary': 'Adds Correos Express Webservices',
