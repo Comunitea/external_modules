9db940ca7061613ec8c975e0022974ea78b6df90
javierjcf <javier@comunitea.com>
2025-10-13 17:58:04 +0200

    [FIX]delivery_correos_express,delivery_nacex: No ejecutar rename_module inexistente

--- delivery_nacex/migrations/18.0.0.0/pre-migration.py
+++ delivery_nacex/migrations/18.0.0.0/pre-migration.py
@@ -3,4 +3,5 @@ from openupgradelib import openupgrade
 # Renombrar el módulo "old_module" a "new_module"
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_module(env.cr, 'delivery_carrier_label_ncx', 'delivery_nacex')
+    return
+    # openupgrade.rename_module(env.cr, 'delivery_carrier_label_ncx', 'delivery_nacex')
