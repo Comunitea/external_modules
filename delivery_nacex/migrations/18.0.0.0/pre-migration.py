6969daf2dadffd3fe117a8db3ee29288ca6c5cbd
Vicente <vicente@comunitea.com>
2025-10-14 15:38:37 +0200

    [MIG] delivery_nacex, delivery_correos_express: se eliminan carpetas de migración

--- delivery_nacex/migrations/18.0.0.0/pre-migration.py
+++
@@ -1,8 +0,0 @@
-from odoo.upgrade import util
-
-# Renombrar el módulo "old_module" a "new_module"
-@openupgrade.migrate()
-def migrate(env, version):
-    module = env['ir.module.module'].search([('name', '=', 'delivery_carrier_label_ncx')], limit=1)
-    if module and module.state not in ['installed', 'to install']:
-        util.rename_module(env.cr, 'delivery_carrier_label_ncx', 'delivery_nacex')
