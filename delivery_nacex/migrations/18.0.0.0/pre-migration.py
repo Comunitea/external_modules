19da6edb670a44fbe7ebc695463e3748a1330664
Vicente <vicente@comunitea.com>
2025-10-14 08:52:34 +0200

    [MIG] delivery_nacex, delivery_correos_express: prueba migration

--- delivery_nacex/migrations/18.0.0.0/pre-migration.py
+++ delivery_nacex/migrations/18.0.0.0/pre-migration.py
@@ -1,7 +1,8 @@
-from openupgradelib import openupgrade
+from odoo.upgrade import util
 
 # Renombrar el módulo "old_module" a "new_module"
 @openupgrade.migrate()
 def migrate(env, version):
-    return
-    # openupgrade.rename_module(env.cr, 'delivery_carrier_label_ncx', 'delivery_nacex')
+    module = env['ir.module.module'].search([('name', '=', 'delivery_carrier_label_ncx')], limit=1)
+    if module and module.state not in ['installed', 'to install']:
+        util.rename_module(env.cr, 'delivery_carrier_label_ncx', 'delivery_nacex')
