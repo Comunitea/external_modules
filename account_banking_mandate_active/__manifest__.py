714eb3ed785d29f5eae6879c6a870226e6e5362a
UxioMendez <uxio.mendev@gmail.com>
2025-10-20 13:08:07 +0200

    [ADD]account_banking_mandate_active

---
+++ account_banking_mandate_active/__manifest__.py
@@ -0,0 +1,16 @@
+# © 2016 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Active Banking mandates",
+    "description": """
+       Allows to deactive banking mandates
+    """,
+    "version": "18.0.1.0.0",
+    "author": "Comunitea",
+    'license': 'AGPL-3',
+    "website": "https://www.comunitea.com",
+    "depends": ["account_banking_mandate"],
+    "category": "Accounting",
+    "data": ['views/account_banking_mandate_view.xml'],
+    'installable': True,
+}
