22048ff4a2761c6f0529500343e0fb862a424121
Vicente <vicente@comunitea.com>
2026-01-28 14:47:43 +0100

    [FIX] delivery_nacex:  service + correciones

--- delivery_nacex/models/stock_picking.py
+++ delivery_nacex/models/stock_picking.py
@@ -30,5 +30,4 @@ class StockPicking(models.Model):
     shipment_reference = fields.Char("Shipment Reference")
     failed_shipping = fields.Boolean("Failed Shipping", default=False)
     delivery_type = fields.Selection(related="carrier_id.delivery_type")
-    delivery_note = fields.Char(compute="_compute_delivery_note")
     ncx_shipping_return = fields.Boolean("Shipping with return", default=False)
